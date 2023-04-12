e9180cc2f98fbbd63f3dcba198062b9d2dda6e7d
reDpz <sv_redpz@proton.me>
2023-04-12 14:13:02 +0100

    updated redpz

--- original_config.h
+++ original_config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -13,11 +13,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono NFM:size=13" };
 static const char dmenufont[]       = "JetBrainsMono NFM:size=13";
-static const char col_gray1[]       = "#2A2A37";
-static const char col_gray2[]       = "#363646";
-static const char col_gray3[]       = "#54546D";
-static const char col_gray4[]       = "#1F1F28";
-static const char col_cyan[]        = "#E6C384";
+static const char col_gray1[]       = "#24283b";
+static const char col_gray2[]       = "#414868";
+static const char col_gray3[]       = "#565f89";
+static const char col_gray4[]       = "#1a1b26";
+static const char col_cyan[]        = "#7dcfff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
